3662d9d796d25096dc5f24fea6fe244e227dd664
Fraser233 <96560495+Fraser233@users.noreply.github.com>
2022-01-16 15:40:52 -0800

    Delete CV-main directory

--- CV-main/index.js
+++
@@ -1,8 +0,0 @@
-function titleShowUp() {
-  var showUp = document.getElementById("subtitleImg");
-  let width = screen.width;
-
-  if (width < 1080px)
-    showUp.style.display = "block";
-  else showUp.style.display = "none";
-}
